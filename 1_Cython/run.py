03178ab111c8c0abdc1818b5e6eef9a4bca13a43
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-01-22 09:21:48 +0100

    update setup & run

--- 1_Cython/run.py
+++ 1_Cython/run.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#coding: utf-8
+
 import numpy
 import inside_polygon
 import time
@@ -8,27 +11,27 @@ if __name__ == "__main__":
     vertices = inside_polygon.make_vertices(n)
     print vertices
 
-#     t0 = time.time()
-#     res = []
-#     for x in range(LEN):
-#         res.append([inside_polygon.insidePolygon(vertices, (x, y)) for y in range(LEN)])
-#     print("execution time Cython: %.3fs" % (time.time() - t0))
-#
+    t0 = time.time()
+    res = []
+    for x in range(LEN):
+        res.append([inside_polygon.inside_polygon(vertices, (x, y)) for y in range(LEN)])
+    print("execution time Cython: %.3fs" % (time.time() - t0))
+
     vertices = numpy.array(vertices).astype(numpy.float32)
 #     print vertices
     poly = inside_polygon.Polygon(vertices)
-#
-#     t0 = time.time()
-#     res = []
-#     for x in range(LEN):
-#         res.append([inside_polygon.insidePolygon_np(vertices, (x, y)) for y in range(LEN)])
-#     print("execution time Cython+Numpy+Opt: %.3fs" % (time.time() - t0))
-#
-#     t0 = time.time()
-#     res = []
-#     for x in range(LEN):
-#         res.append([poly.isInside(x, y) for y in range(LEN)])
-#     print("execution time Cython+Numpy+class+Opt: %.3fs" % (time.time() - t0))
+
+    t0 = time.time()
+    res = []
+    for x in range(LEN):
+        res.append([inside_polygon.inside_polygon_np(vertices, (x, y)) for y in range(LEN)])
+    print("execution time Cython+Numpy+Opt: %.3fs" % (time.time() - t0))
+
+    t0 = time.time()
+    res = []
+    for x in range(LEN):
+        res.append([poly.isInside(x, y) for y in range(LEN)])
+    print("execution time Cython+Numpy+class: %.3fs" % (time.time() - t0))
     t0 = time.time()
     msk = poly.make_mask(LEN, LEN)
     print("execution time Cython+Numpy+class+Opt+loop: %.3fs" % (time.time() - t0))
