e2ed85ae1bb3f647095abb00b118cf06ae7549aa
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-01-22 09:29:50 +0100

    add setup (even if not really needed)

---
+++ 0_Python/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+from Cython.Distutils import build_ext
+from distutils.extension import Extension
+
+cy_mod = Extension("inside_polygon",
+	sources=	["inside_polygon.pyx"])
+
+setup(ext_modules=[cy_mod],
+	cmdclass={'build_ext': build_ext})
