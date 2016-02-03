20d9ddd107b46f29ae3f4d41bfe9394689f251a5
Jerome Kieffer <jerome.kieffer@esrf.fr>
2016-02-03 08:05:31 +0100

    add comments regarding numpy & windows

--- 0_Python/setup.py
+++ 0_Python/setup.py
@@ -4,8 +4,12 @@ from distutils.core import setup
 from Cython.Distutils import build_ext
 from distutils.extension import Extension
 
+# Under windows, numpy include file needs to be provided.
+# from numpy.distutils.core import Extension
+
 cy_mod = Extension("inside_polygon",
-	sources=	["inside_polygon.pyx"])
+                   sources=["inside_polygon.pyx"],
+                   )
 
 setup(ext_modules=[cy_mod],
 	cmdclass={'build_ext': build_ext})
